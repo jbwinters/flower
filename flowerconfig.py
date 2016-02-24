6e176ad4435f98ec858d8e5337f626b63ff96813
Josh Winters <josh@idealspot.com>
2016-02-24 17:40:00 -0600

    Optional CELERY_BROKER_URL

--- flowerconfig.py
+++ flowerconfig.py
@@ -3,8 +3,8 @@ import os
 from flower.utils.template import humanize
 
 
-BROKER_URL = os.environ['CELERY_BROKER_URL']
-
+if 'CELERY_BROKER_URL' in os.environ:
+    BROKER_URL = os.environ['CELERY_BROKER_URL']
 
 def format_task(task):
     task.args = humanize(task.args, length=50)
